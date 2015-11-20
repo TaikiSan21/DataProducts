c3f3237fc6f3d6575151412f1b896832b7662898
TaikiSan <tnsakai@gmail.com>
2015-11-19 16:00:36 -0800

    Edited server file

--- server.R
+++ server.R
@@ -21,18 +21,4 @@ shinyServer(
       }
 )
             
-            # ## Testing
-            # # y = a + x(b+cz) + dz
-            # b <- 2
-            # c <- -2
-            # d <- 1
-            # z <- 2
-            # a <- 0
-            # int <- a + d*z
-            # slp <- b + c*z
-            # manipulate( {ggplot(data.frame(x=c(-20,20)), aes(x)) +
-            #       stat_function(fun=function(x) exp((a + d*z) + (b + c*z)*x)/(1+exp((a + d*z) + (b + c*z)*x))) +
-            #             coord_cartesian(xlim=c(-20,20),ylim=c(-.1,1.1)) +
-            #             labs(title='Y = a+dz + x(b+cz)')},
-            #       d=slider(-2,2,step=.1,initial=0), z=slider(-2,2,step=.1,initial=0), c=slider(-2,2,step=.1,initial=0),b=slider(-2,2,step=.1,initial=0))
-            
+      
